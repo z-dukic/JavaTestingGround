4238cb9f3596d790b1846eb7858a22e096be715c
Zoran Đukić <zorandjukic.os@gmail.com>
2021-06-04 22:59:52 +0200

    Jednostavna vrsta podataka

---
+++ src/Testing01.java
@@ -0,0 +1,29 @@
+package testing01;
+
+import javax.swing.JOptionPane;
+
+public class Testing01 {
+
+	public static void main(String[] args) {
+		/*int prviBroj, drugiBroj;
+		prviBroj = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		drugiBroj = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
+		System.out.println(prviBroj + drugiBroj);
+		
+		String pero = "Di si pero";
+		System.out.println(pero.toUpperCase());
+		*/
+		
+		int a,b,c;
+		a = Integer.parseInt(JOptionPane.showInputDialog("Daj jedan broj"));
+		b = Integer.parseInt(JOptionPane.showInputDialog("Daj još jedan"));
+		c = Integer.parseInt(JOptionPane.showInputDialog("Daj broj tri"));
+		System.out.println(a + b + c);
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/Testing02.java
@@ -0,0 +1,28 @@
+package testing01;
+
+public class Testing02 {
+
+	public static void main(String[] args) {
+		int a = 0;
+
+		boolean b = false;
+		System.out.println(b);
+
+		boolean c = a == 0;
+		System.out.println(c);
+
+		boolean tocanRezultat = true;
+		boolean netocanRezultat = false;
+
+		if (tocanRezultat) {
+			System.out.println("Rezultat je tocan");
+			
+			if(netocanRezultat) {
+				System.out.println("Rezultat nije tocan");
+				
+			};
+
+
+		}
+	}
+}
