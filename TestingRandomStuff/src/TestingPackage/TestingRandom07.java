08ab047c8e4bae880f39dd67250ab706d83bd540
Zoran Đukić <zorandjukic.os@gmail.com>
2021-06-07 11:18:30 +0200

    Zbrajanje, oduzimanje

--- TestingRandomStuff/src/TestingPackage/TestingRandom07.java
+++ TestingRandomStuff/src/TestingPackage/TestingRandom07.java
@@ -6,12 +6,13 @@ public class TestingRandom07 {
 
 	public static void main(String[] args) {
 		
-		//Sve osim ako napišeš true izbacit æe false. Bilo koji broj, rijeè itd.
+		//Sve osim ako napišeš true izbacit æe false. Bilo koji broj, rijeè itd. 
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Napiši jedan boolean");
 		boolean value = Boolean.valueOf(scanner.nextLine());
 		System.out.println("Napisao si: " + value);
-
+		
+		
 	}
 
 }
---
+++ TestingRandomStuff/src/TestingPackage/TestingRandom08.java
@@ -0,0 +1,26 @@
+package TestingPackage;
+
+import java.util.Scanner;
+
+public class TestingRandom08 {
+
+	public static void main(String[] args) {
+		
+		//Bez zagrada ponaša se kao dva string
+		
+		System.out.println("Rezultat je: " + 2 + 2);
+		System.out.println("Rezultat je: " + (2 + 2));
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Daj mi broj: ");
+		int prviBroj = Integer.valueOf(scanner.nextLine());
+		System.out.println("Daj mi drugi broj: ");
+		int drugiBroj = Integer.valueOf(scanner.nextLine());
+		
+		System.out.println(prviBroj + drugiBroj);
+		System.out.println("Zbrajanje: " + (prviBroj + drugiBroj));
+		
+
+	}
+
+}
