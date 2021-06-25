6576317e55e2e7823f9e8e28e3c17904bcc975b5
Zoran Đukić <zorandjukic.os@gmail.com>
2021-06-25 23:14:35 +0200

    If

--- TestingRandomStuff/src/TestingPackage/TestingRandom64.java
+++ TestingRandomStuff/src/TestingPackage/TestingRandom64.java
@@ -4,17 +4,6 @@ import javax.swing.JOptionPane;
 
 public class TestingRandom64 {
 
-	public static void main(String[] args) {
 
-		// int x = Integer.parseInt(JOptionPane.showInputDialog("Broj stupaca"));
-		// int y = Integer.parseInt(JOptionPane.showInputDialog("Broj redova"));
-
-		  int rows = 5;
-
-		    // outer loop
-		    for (int i = 1; i <= rows; ++i) {
-
-		}
-
-	}
+}
 
--- TestingRandomStuff/src/TestingPackage/TestingRandom65.java
+++ TestingRandomStuff/src/TestingPackage/TestingRandom65.java
@@ -11,24 +11,35 @@ public class TestingRandom65 {
 		int sum = 0;
 		int sum1 = 0;
 		int sum2 = 0;
+		int sum3 = 0;
 
 		int stupacX = x;
 		int redY = y;
 
 		int[][] array = new int[x][y];
 		for (int i = 0; i < stupacX; i++) {
-			for (int j = 1; j < redY; j++) {
+			for (int j = 0; j < redY; j++) {
 				sum = (sum + 1);
 				sum1 = x * y;
 				sum2 = sum1 - sum;
-				
+				sum3 = ++sum2;
 
 				array[i][j] = sum2;
-				System.out.print(" " + array[i][j]);
-			}
-			System.out.println("");
-		}
+				System.out.println(" " + sum2);
+				
+				
+				if(sum2%x==0 && sum2%y==0) {
+					System.out.println("PEro");
+					System.out.println(" " + sum2);
+				
 
+			}
+
+				
+			}
+
+		}
+		System.out.println("");
 	}
 
 }
